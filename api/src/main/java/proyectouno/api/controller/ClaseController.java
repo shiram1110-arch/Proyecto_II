a5735cd6fc2ff66c72d057104a18a8c69b8ee5ef
Luis Calderón <hawkcr20@gmail.com>
2026-04-14 18:41:38 -0600

    primer commit

---
+++ api/src/main/java/proyectouno/api/controller/ClaseController.java
@@ -0,0 +1,65 @@
+package proyectouno.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import proyectouno.api.entity.Clase;
+import proyectouno.api.service.ClaseService;
+
+@CrossOrigin(origins = "*") // Permitir acceso desde cualquier origen
+@Tag(name = "Clases", description = "API para gestionar clases")
+// Grupo en Swagger
+@RestController
+@RequestMapping("/clases")
+
+public class ClaseController {
+    @Autowired
+    private ClaseService claseService;
+
+    @GetMapping
+    @Operation(summary = "Obtener todas las clases", description = "Devuelve una lista de clases")
+    public List<Clase> get() {
+        return claseService.get();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Obtener una clase por ID", description = "Busca una clase en la base de datos según su ID")
+
+    public Clase getById(@PathVariable int id) {
+        return claseService.getById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    @Operation(summary = "Crear una nueva clase", description = "Agrega una nueva clase a la base de datos")
+
+    public Clase add(@RequestBody Clase clase) {
+        return claseService.add(clase);
+    }
+
+    @Operation(summary = "Modificar una clase", description = "Modifica una  clase existente en la base de datos")
+    @PutMapping("/{id}")
+    public Clase update(@PathVariable int id, @RequestBody Clase clase) {
+        return claseService.update(id, clase);
+    }
+
+    @Operation(summary = "Eliminar una clase", description = "Elimina una clase de la base de datos")
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        claseService.delete(id);
+    }
+
+}
--- api/src/main/java/proyectouno/api/controller/ReservaController.java
+++ api/src/main/java/proyectouno/api/controller/ReservaController.java
@@ -13,43 +13,44 @@ import proyectouno.api.service.*;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@CrossOrigin(origins = "*") // Permitir acceso desde cualquier origen
+@Tag(name = "Reservas", description = "API para gestionar reservas")
+// Grupo en Swagger
+@RestController
+@RequestMapping("/reservas")
+public class ReservaController {
+    @Autowired
+    private ReservaService reservaService;
 
+    @GetMapping
+    @Operation(summary = "Obtener todas las reservas", description = "Devuelve una lista de reservas")
+    public List<Reserva> get() {
+        return reservaService.get();
+    }
 
-@CrossOrigin(origins = "*") // Permitir acceso desde cualquier origen 
-@Tag(name = "Reservas", description = "API para gestionar reservas") 
-// Grupo en Swagger 
-@RestController 
-@RequestMapping("/reservas") 
-public class ReservaController { 
-    @Autowired 
-    private ReservaService reservaService; 
-    @GetMapping 
-    @Operation(summary = "Obtener todas las reservas", description = "Devuelve una lista de reservas") 
-    public List<Reserva> get() { 
-        return reservaService.get(); 
-    } 
-    @GetMapping("/{id}") 
-    @Operation(summary = "Obtener una reserva por ID", description = "Busca una reserva en la base de datos según su ID") 
- 
-public Reserva getById(@PathVariable int id) { 
-    return reservaService.getById(id).orElseThrow(() -> new 
-ResponseStatusException(HttpStatus.NOT_FOUND)); 
-} 
-    @PostMapping 
-    @Operation(summary = "Crear una nueva reserva", description = "Agrega una nueva reserva a la base de datos") 
- 
-    public Reserva add(@RequestBody Reserva reserva) { 
-        return reservaService.add(reserva); 
-    } 
-    @Operation(summary = "Modificar una reserva", description = "Modifica una  reserva existente en la base de datos") 
-    @PutMapping("/{id}") 
-    public Reserva update(@PathVariable int id, @RequestBody Reserva 
-reserva) { 
-        return reservaService.update(id,reserva); 
-    } 
-    @Operation(summary = "Eliminar una reserva", description = "Elimina una reserva de la base de datos") 
-    @DeleteMapping("/{id}") 
-    public void delete(@PathVariable int id) { 
-        reservaService.delete(id); 
-    } 
-} 
+    @GetMapping("/{id}")
+    @Operation(summary = "Obtener una reserva por ID", description = "Busca una reserva en la base de datos según su ID")
+
+    public Reserva getById(@PathVariable int id) {
+        return reservaService.getById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    @Operation(summary = "Crear una nueva reserva", description = "Agrega una nueva reserva a la base de datos")
+
+    public Reserva add(@RequestBody Reserva reserva) {
+        return reservaService.add(reserva);
+    }
+
+    @Operation(summary = "Modificar una reserva", description = "Modifica una  reserva existente en la base de datos")
+    @PutMapping("/{id}")
+    public Reserva update(@PathVariable int id, @RequestBody Reserva reserva) {
+        return reservaService.update(id, reserva);
+    }
+
+    @Operation(summary = "Eliminar una reserva", description = "Elimina una reserva de la base de datos")
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        reservaService.delete(id);
+    }
+}
